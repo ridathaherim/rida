38011eeb402496a21cc52fa1331f5fb8171a541f
ridathaherim <163110413+ridathaherim@users.noreply.github.com>
2024-03-12 13:58:42 +0530

    Add files via upload

---
+++ Divisibleby.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Divisibleby {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        if(n%5==0 && n%11==0){
+            System.out.println("yes,"+n+" is divisible by 5 and 11");
+        }
+        else{
+            System.out.println("Not divisible by 5 and 11");
+        }
+    }
+}
