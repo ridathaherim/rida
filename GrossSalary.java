0bd6c25c1aef5b02dca9bc7cba0856a2b88295ba
ridathaherim <163110413+ridathaherim@users.noreply.github.com>
2024-03-12 18:43:37 +0530

    Update GrossSalary.java

--- GrossSalary.java
+++ GrossSalary.java
@@ -1,3 +1,16 @@
+/*
+Write a program to input basic salary of an employee and calculate its Gross salary according to following:
+
+Basic Salary <= 10000 : HRA = 20%, DA = 80%
+Basic Salary <= 20000 : HRA = 25%, DA = 90%
+Basic Salary > 20000 : HRA = 30%, DA = 95%
+
+I/P
+Enter The Basic Salary :25000
+
+O/P
+Gross Salary : 56250.0
+*/
 import java.util.Scanner;
 
 public class GrossSalary {
