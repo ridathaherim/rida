94a5a85dab7ced79d56c75c6318984da42250758
ridathaherim <163110413+ridathaherim@users.noreply.github.com>
2024-03-12 18:44:41 +0530

    Update MaxNum.java

--- MaxNum.java
+++ MaxNum.java
@@ -1,3 +1,25 @@
+/*
+Write a program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+Percentage >= 90% : Grade A
+Percentage >= 80% : Grade B
+Percentage >= 70% : Grade C
+Percentage >= 60% : Grade D
+Percentage >= 40% : Grade E
+Percentage < 40% : Grade F
+
+I/P
+Enter The Five Subject Marks :
+65
+89
+82
+95
+85
+
+O/P
+Total :416
+Percentage :83.0
+Grade B
+*/
 import java.util.Scanner;
 
 public class MaxNum {
