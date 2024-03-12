86df9b334889eae85bbcc8328bd5891ae248895d
ridathaherim <163110413+ridathaherim@users.noreply.github.com>
2024-03-12 18:49:09 +0530

    Update TypesOfTriangles.java

--- TypesOfTriangles.java
+++ TypesOfTriangles.java
@@ -1,3 +1,16 @@
+/*8) Write a program to check whether the triangle is equilateral, isosceles or scalene triangle
+Logic:
+If s1, s2 and s3 are 3 sides of the triange
+when all 3 sides are equal -> Equilateral Triangle
+When any 2 sides are euqal -> Isosceles Triangle
+Otherwise Scalene Triangle
+
+I/P
+Enter 3 sides of the triangle: 60 45 45
+
+O/P
+Isosceles Triangle.
+*/
 import java.util.Scanner;
 
 public class TypesOfTriangles {
