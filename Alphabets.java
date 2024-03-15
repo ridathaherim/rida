2937bba6fb81741aa7ea2efe541cd0531a8cc4b8
ridathaherim <163110413+ridathaherim@users.noreply.github.com>
2024-03-16 00:45:11 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,12 @@
+/*Write a program to print all alphabets from a to z
+O/P
+        abcdefghijklmnopqrstuvwxyz
+        */
+public class Alphabets {
+    public static void main(String []args){
+        char i='a';
+        for(;i<='z';i++){
+            System.out.print(i);
+        }
+    }
+}
